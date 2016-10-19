c4a9ada29ce6c73378a18ce7270f7d962965444a
rishabhkumar <rishabh.jarvis@gmail.com>
2016-10-20 02:54:33 +0530

    New index page and corrections on index1

--- index.js
+++ index.js
@@ -6,8 +6,23 @@
 
 
 $(function () {
-    let h = 420, h1 = 250, w1 = 250, w2 = 15;
-    $('.leftPanel').css({'width' : '15%'})
+    initCSS()
+    $('.ramu').click(function (event) {
+        $('#me_left').attr('class', 'collection-item')
+        $('#cont_left').attr('class', 'collection-item')
+        $('#skills_left').attr('class', 'collection-item')
+        $('#dev_left').attr('class', 'collection-item')
+        $(event.target).attr('class', 'collection-item active')
+        let tempID = '#' + event.target.id.split('_')[0] + 'Content';
+        $('.cont').hide()
+        $(tempID).show()
+    })
+})
+
+function initCSS() {
+    $('.cont').hide()
+    let h = 680, h1 = 250, w1 = 250, w2 = 15;
+    $('.leftPanel').css({'width' : '15%', 'height' : '90%'})
     $('.centerPanel').css({'width': '80%', 'margin_right': '3%'})
     if(screen.width < 400) {
         h = 200
@@ -19,21 +34,9 @@ $(function () {
     }
     $('#rk').css({'height': h1,'width': w1})
     $('.slider').slider({full_width: true, height: h});
-    $('.ramu').click(function (event) {
-        $('#me_left').attr('class', 'collection-item')
-        $('#cont_left').attr('class', 'collection-item')
-        $('#skills_left').attr('class', 'collection-item')
-        $('#dev_left').attr('class', 'collection-item')
-        $(event.target).attr('class', 'collection-item active')
-        if(event.target.id == 'me_left') {
-            $('.lol2').show()
-            $('.slider').slider({full_width: true, height: h});
-        }
-        else if(event.target.id == 'skills_left') {
-            $('.lol2').hide()
-        }
-    })
-})
+}
+
+
 
 
 
---
+++ main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by rishabh on 20/10/16.
+ */
+
+
+$(function () {
+    setTimeout(function(){ window.open('index1.html', '_self'); }, 2000);
+})
