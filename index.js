1ea3d6138cd137335932ecd4c3ecb2594b5a6aa7
rishabhkumar <rishabh.jarvis@gmail.com>
2016-10-19 20:08:28 +0530

    Correcting Structure

--- index.js
+++ index.js
@@ -6,7 +6,19 @@
 
 
 $(function () {
-    $('.slider').slider({full_width: true, height: 500});
+    let h = 420, h1 = 250, w1 = 250, w2 = 15;
+    $('.leftPanel').css({'width' : '15%'})
+    $('.centerPanel').css({'width': '80%', 'margin_right': '3%'})
+    if(screen.width < 400) {
+        h = 200
+        h1 = 125
+        w1 = 100
+        w2 = 35
+        $('.leftPanel').css({'width' : '30%'})
+        $('.centerPanel').css({'width': '63%', 'margin_right': '2%'})
+    }
+    $('#rk').css({'height': h1,'width': w1})
+    $('.slider').slider({full_width: true, height: h});
     $('.ramu').click(function (event) {
         $('#me_left').attr('class', 'collection-item')
         $('#cont_left').attr('class', 'collection-item')
@@ -15,7 +27,7 @@ $(function () {
         $(event.target).attr('class', 'collection-item active')
         if(event.target.id == 'me_left') {
             $('.lol2').show()
-            $('.slider').slider({full_width: true, height: 500});
+            $('.slider').slider({full_width: true, height: h});
         }
         else if(event.target.id == 'skills_left') {
             $('.lol2').hide()
@@ -23,11 +35,7 @@ $(function () {
     })
 })
 
-// function lol() {
-//     alert('loliy' + $(this).attr('id'))
-//     $('#me_left').attr('class', 'collection-item')
-//     $('#cont_left').attr('class', 'collection-item')
-//     $('#skills_left').attr('class', 'collection-item')
-//     $('#dev_left').attr('class', 'collection-item')
-//     $(this).attr('class', 'collection-item active')
-// }
+
+
+
+
